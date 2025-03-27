aa3318e961a70acf7cc6da2c47e0a1951f2aec6e
Matt Frewin <matthew.frewin@outlook.com>
2025-03-27 15:50:23 +0000

    Few additions to battle-sim
    Random attack phrases, round announcements, await input to slow down execution for user's to follow the battle

--- Levels 1 to 5/Level 2.2.py
+++ Levels 1 to 5/Level 2.2.py
@@ -30,17 +30,27 @@ class Character:
             print(f"Hells Bells! {self.get_name()} let out a battle cry and lands a whopper on {target.get_name()}.")
             target.take_damage(self.strength * 2)
         else:
-            print(f"{self.get_name()} says 'Have some of this!' and attacks {target.get_name()}.")
+            random_phrases = [
+                f"{self.get_name()} says 'Have some of this!' and attacks {target.get_name()}.",
+                f"{self.get_name()} whips their biscuit and tries to cause havoc for {target.get_name()}.",
+                f"{self.get_name()} has fire in there belly and smashes into {target.get_name()}.",
+                f"{self.get_name()} screams 'I'll teach you some manners!' and throws themself at {target.get_name()}."
+            ]
+            print(random.choice(random_phrases))
             target.take_damage(self.strength)
 
 # Battle of the beasts...
 beholder = Character("Frank the Beholder", 200, 6)
 dragon = Character("Doris the Dragon", 120, 19)
 
+turn = 1
 while beholder.is_alive() and dragon.is_alive():
+    print(f"Round {turn}:")
     beholder.attack(dragon)
     print()
     dragon.attack(beholder)
     print()
+    turn += 1
+    input("Press Enter key to continue...")
 
 
